afe9cb391cb1588e69f9b26e4bf8b38a2dd09a3a
Andrec1350 <114308210+AndreiCusiac@users.noreply.github.com>
2023-03-25 16:29:49 +0200

    Added some preprocessing methods

--- tests/preprocessing_data.py
+++ tests/preprocessing_data.py
@@ -1,8 +1,20 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
-DATA_PATH = 'data/Environment_Temperature_change_E_All_Data_NOFLAG.csv'
+DATA_PATH = '../data/Environment_Temperature_change_E_All_Data_NOFLAG.csv'
 
 data_frame = pd.read_csv(DATA_PATH, encoding='cp1252')
-print(data_frame.shape)
-print(data_frame.head(10))
+#print(data_frame.shape)
+#print(data_frame.head(10))
+
+scaling_columns = ['Y' + str(i) for i in range(1961,2020)]
+
+# print(data_frame[scaling_columns])
+# data_frame[scaling_columns] = StandardScaler().fit_transform(data_frame[scaling_columns].values)
+# print(data_frame[scaling_columns])
+
+area_code_column = ['Area Code']
+
+print(data_frame[area_code_column])
+print(LabelEncoder().fit_transform(data_frame[area_code_column].values))
+print(data_frame[area_code_column])
